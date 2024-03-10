4d92fddbb691ac242e9d6f8e04239cacebc9dd7d
_aion <dorian.molart@gmail.com>
2024-03-10 20:38:21 +0100

    Example with ECS

--- sandbox/src/engine.ts
+++ sandbox/src/engine.ts
@@ -1,9 +1,26 @@
+import { aion, defineComponent, i32 } from "aion-ecs/src";
 import { defineEngine, defineLoop, emit, on } from "aion-engine";
-import { getMouseX, getMouseY, initInputListener } from "aion-input";
-import { initWindow } from "aion-render";
+import { axis, getMouseX, getMouseY, initInputListener } from "aion-input";
+import { initWindow, rect } from "aion-render";
 import { clear, circle, Colors } from "aion-render";
 
 const run = defineEngine(() => {
+  const { prefab } = aion();
+
+  const Position = defineComponent({
+    x: i32,
+    y: i32,
+  });
+
+  const Velocity = defineComponent({
+    x: i32,
+    y: i32,
+  });
+
+  const createPlayer = prefab({ Position, Velocity });
+
+  const player = createPlayer();
+
   on("boot", () => {
     initWindow();
     initInputListener();
@@ -11,11 +28,22 @@ const run = defineEngine(() => {
 
   defineLoop(() => {
     clear();
+    emit("update");
     emit("draw");
   });
 
   on("draw", () => {
     circle(getMouseX(), getMouseY(), 100).fill(Colors["acapulco:500"]);
+
+    rect(Position.x[player], Position.y[player], 100, 100).stroke("black");
+  });
+
+  on("update", () => {
+    Velocity.x[player] = axis("horizontal") * 4;
+    Velocity.y[player] = axis("vertical") * 4;
+
+    Position.x[player] += Velocity.x[player];
+    Position.y[player] += Velocity.y[player];
   });
 });
 
