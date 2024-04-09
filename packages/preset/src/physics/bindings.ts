7ba3c3beea7b456b082737deebe2588a08863991
_aion <dorian.molart@gmail.com>
2024-04-09 08:19:38 +0200

    Reorganize some collider logic

--- packages/preset/src/physics/bindings.ts
+++ packages/preset/src/physics/bindings.ts
@@ -1,15 +1,12 @@
-import { onEnterQuery, not, type Entity } from "aion-ecs";
+import { onEnterQuery, not } from "aion-ecs";
 import {
   Body,
   Collider,
   RuntimeBody,
   RuntimeCollider,
-  mapColliderHandleToEntity,
-  setColliderOptions,
+  createRuntimeCollider,
   usePhysics,
 } from "./index.js";
-import type RAPIER from "@dimforge/rapier2d-compat";
-import { Circle, Rect } from "../components.js";
 import { useECS } from "../ecs.js";
 import { Vec, type Vector } from "aion-core";
 import {
@@ -52,27 +49,12 @@ export function initPhysicsSystems() {
   onCreatedColliderWithRuntimeBody((ent) => {
     let body = getRuntimeBody(ent);
 
-    const auto = Collider.auto[ent];
-
-    if (!auto) {
-      throw new Error("not implemented");
-    }
-
-    const collidersDesc = createColliderDesc(ent);
-
-    const colliderDesc = collidersDesc[0]!;
-
-    setColliderOptions(colliderDesc, ent);
-
-    const collider = world.createCollider(colliderDesc, body);
-
-    mapColliderHandleToEntity(collider.handle, ent);
+    const collider = createRuntimeCollider(ent, world, body);
 
     collider.setTranslation(toSimulationPoint(getWorldPosition(ent)));
     collider.setRotation(getWorldRotation(ent));
 
     RuntimeCollider[ent] = collider;
-
     attach(RuntimeCollider, ent);
   });
 
@@ -86,59 +68,18 @@ export function initPhysicsSystems() {
 
     traverseDescendants(entity, (ent) => {
       if (has(Collider, entity)) {
-        const auto = Collider.auto[ent];
-
-        if (!auto) {
-          throw new Error("not implemented");
-        }
-
-        const collidersDesc = createColliderDesc(ent);
-
-        const colliderDesc = collidersDesc[0]!;
-
-        setColliderOptions(colliderDesc, ent);
-
-        const collider = world.createCollider(colliderDesc, body);
+        const collider = createRuntimeCollider(ent, world, body);
 
         collider.setTranslationWrtParent(
           toSimulationPoint(getLocalPosition(ent)),
         );
-
         collider.setRotationWrtParent(getLocalRotation(ent));
 
-        mapColliderHandleToEntity(collider.handle, ent);
-
         RuntimeCollider[ent] = collider;
-
         attach(RuntimeCollider, ent);
       }
     });
   });
-}
-
-function createColliderDesc(ent: Entity): RAPIER.ColliderDesc[] {
-  const { has } = useECS();
-  const { RAPIER } = usePhysics();
-
-  const colliders: RAPIER.ColliderDesc[] = [];
-
-  if (has(Circle, ent)) {
-    const radius = Circle.r[ent]!;
-    const collider = RAPIER.ColliderDesc.ball((radius / SCALE_FACTOR) | 0);
-    colliders.push(collider);
-  }
-
-  if (has(Rect, ent)) {
-    const w = Rect.w[ent]!;
-    const h = Rect.h[ent]!;
-    const collider = RAPIER.ColliderDesc.cuboid(
-      w / 2 / SCALE_FACTOR,
-      h / 2 / SCALE_FACTOR,
-    );
-    colliders.push(collider);
-  }
-
-  return colliders;
 }
 
 export const SCALE_FACTOR = 50;
--- packages/preset/src/physics/colliders.ts
+++ packages/preset/src/physics/colliders.ts
@@ -1,6 +1,10 @@
 import { SparseSet, type Entity, type PrefabInstanceOptions } from "aion-ecs";
 import { Collider, RuntimeCollider } from "./components.js";
 import RAPIER from "@dimforge/rapier2d-compat";
+import { Circle, Rect } from "../components.js";
+import { useECS } from "../ecs.js";
+import { SCALE_FACTOR } from "./bindings.js";
+import { usePhysics } from "./init.js";
 
 // @todo: these seems to be the defaults of RAPIER but it does not work.
 // Collider init is not ideal
@@ -68,20 +72,70 @@ export function setColliderOptions(
   // colliderDesc.rotationsEnabled = Boolean(Collider.rotationsEnabled[entity]!);
 }
 
-export const COLLIDERS_HANDLE_TO_ENTITY_ID = new Map<number, Entity>();
+export function createRuntimeCollider(
+  ent: number,
+  world: RAPIER.World,
+  body?: RAPIER.RigidBody,
+) {
+  const auto = Collider.auto[ent];
 
-export function mapColliderHandleToEntity(handle: number, entity: Entity) {
-  COLLIDERS_HANDLE_TO_ENTITY_ID.set(handle, entity);
+  if (!auto) {
+    throw new Error("not implemented");
+  }
+
+  const collidersDesc = createColliderDesc(ent);
+
+  const colliderDesc = collidersDesc[0]!;
+
+  setColliderOptions(colliderDesc, ent);
+
+  const collider = world.createCollider(colliderDesc, body);
+
+  mapColliderHandleToEntity(collider.handle, ent);
+
+  return collider;
 }
 
-export function unmapColliderHandleToEntity(handle: number) {
-  COLLIDERS_HANDLE_TO_ENTITY_ID.delete(handle);
-}
+export function createColliderDesc(ent: Entity): RAPIER.ColliderDesc[] {
+  const { has } = useECS();
+  const { RAPIER } = usePhysics();
 
-export function getColliderEntity(handle: number): Entity {
-  return COLLIDERS_HANDLE_TO_ENTITY_ID.get(handle)!;
+  const colliders: RAPIER.ColliderDesc[] = [];
+
+  if (has(Circle, ent)) {
+    const radius = Circle.r[ent]!;
+    const collider = RAPIER.ColliderDesc.ball((radius / SCALE_FACTOR) | 0);
+    colliders.push(collider);
+  }
+
+  if (has(Rect, ent)) {
+    const w = Rect.w[ent]!;
+    const h = Rect.h[ent]!;
+    const collider = RAPIER.ColliderDesc.cuboid(
+      w / 2 / SCALE_FACTOR,
+      h / 2 / SCALE_FACTOR,
+    );
+    colliders.push(collider);
+  }
+
+  return colliders;
 }
 
 export function getRuntimeCollider(entity: Entity) {
   return RuntimeCollider[entity]!;
+}
+
+// is there a more efficient data structure ? An array ?
+export const COLLIDER_HANDLE_TO_ENTITY_ID = new Map<number, Entity>();
+
+export function mapColliderHandleToEntity(handle: number, entity: Entity) {
+  COLLIDER_HANDLE_TO_ENTITY_ID.set(handle, entity);
+}
+
+export function unmapColliderHandleToEntity(handle: number) {
+  COLLIDER_HANDLE_TO_ENTITY_ID.delete(handle);
+}
+
+export function getColliderEntity(handle: number): Entity {
+  return COLLIDER_HANDLE_TO_ENTITY_ID.get(handle)!;
 }
